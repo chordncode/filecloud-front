fb499bf4e6893725faed4688060b205c614382cd
chordncode <chordncode@gmail.com>
2023-09-25 21:04:55 +0900

    file modified

--- src/file/FileList.js
+++ src/file/FileList.js
@@ -1,61 +1,96 @@
 import * as React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, Container, Box, Typography } from '@mui/material';
+import { CssBaseline, Container, Box, Grid, Button, Snackbar, IconButton, Alert } from '@mui/material';
+import { CloseIcon } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
 
 const defaultTheme = createTheme();
-
 export default function FileList() {
     const nav = useNavigate();
     const columns = [
-        { field: 'originalFileName', headerName: '파일명', width: 440 },
-        { field: 'fileSize', headerName: '파일 크기', width: 160 }
+        { field: 'originalFileName', headerName: '파일명', minWidth: 300, flex: 1 },
+        { field: 'fileSize', headerName: '파일 크기', minWidth: 150, flex: 0.4 }
       ];
     const [fileList, setFileList] = React.useState([]);
+    const logout = () => {
+        axios.post('/signout')
+         .then(response => {
+            nav('/');
+         })
+         .catch(error => {
+            openToast();
+         });
+    };
 
     React.useEffect(() => {
         axios.get('/file')
         .then(response => {
             const newFileList = response.data.map(data => ({
                 id: data.fileSn,
-                originalFileName: data.originalFileName,
+                originalFileName: data.originalFileName ? data.originalFileName : data.savedFileName,
                 fileSize: data.fileSize
             }));
             setFileList(newFileList);
         })
         .catch(error => {
-            console.log(error);
+            if(error.response.status === 401) {
+                nav('/');
+            }
         });
-
-        
     }, []);
+
+    const [open, setOpen] = React.useState(false);
+    const openToast = () => {
+        setOpen(true);
+    };
+    const closeToast = () => {
+        setOpen(false);
+    };
+
     return(
         <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
+            <Container component="main" maxWidth="lg">
                 <CssBaseline />
+                <Grid
+                    container
+                    justifyContent="flex-end"
+                >
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{ my: 2 }}
+                        onClick={logout}
+                        >
+                        로그아웃
+                    </Button>
+                </Grid>
                 <Box
                     sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
+                        my: 2,
+                        alignItems: 'center'
                     }}
                 >
                     <DataGrid
+                        autoHeight
                         rows={fileList}
                         columns={columns}
                         initialState={{
-                        pagination: {
-                            paginationModel: { page: 0, pageSize: 5 },
-                        },
+                            pagination: {
+                                paginationModel: { page: 0, pageSize: 10 },
+                            },
                         }}
                         pageSizeOptions={[5, 10]}
                         checkboxSelection
                     />
                 </Box>
             </Container>
+            <Snackbar open={open} autoHideDuration={3000} onClose={closeToast}>
+                <Alert onClose={closeToast} severity="error" sx={{ width: '100%' }}>
+                    잠시 후 다시 시도해주세요.
+                </Alert>
+            </Snackbar>
         </ThemeProvider>
     );
 }
